6ef5f29aa7a2d05c74d7b407e27128e8f2bba74d
chenmichael0414 <85696532+chenmichael0414@users.noreply.github.com>
2025-05-15 14:53:27 -0400

    Created Apollo Client config
    Used api provided in document

---
+++ src/apollo/client.ts
@@ -0,0 +1,7 @@
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { cache } from 'react';
+
+export const client = new ApolloClient({
+    uri: 'https://api.spacex.land/graphql/',
+    cache: new InMemoryCache(),
+});
